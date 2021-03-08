b7c042299668bb9017a53f16b6999819c22cb93d
jthanawat <kla13thanawat@gmail.com>
2021-03-09 00:18:29 +0700

    add Modal component

---
+++ src/globalStyles.js
@@ -0,0 +1,10 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GloblStyle = createGlobalStyle`
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Arial', 'sans-serif';
+}
+`;
