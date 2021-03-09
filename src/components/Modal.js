48ebd3473b347f320934a9338deccff204fdc360
jthanawat <kla13thanawat@gmail.com>
2021-03-09 09:43:14 +0700

    add outside click

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useRef, useEffect, useCallback } from 'react';
+import { useSpring, animated } from 'react-spring';
 import styled from 'styled-components';
 import { MdClose } from 'react-icons/md';
 
@@ -64,25 +65,58 @@ const CloseModalButton = styled(MdClose)`
 `;
 
 const Modal = ({ showModal, setShowModal }) => {
+  const modalRef = useRef();
+
+  const animation = useSpring({
+    config: {
+      duration: 250,
+    },
+    opacity: showModal ? 1 : 0,
+    transform: showModal ? `translateY(0%)` : `translateY(-100%)`,
+  });
+
+  const closeModal = (e) => {
+    if (modalRef.current === e.target) {
+      setShowModal(false);
+    }
+  };
+
+  const keyPress = useCallback(
+    (e) => {
+      if (e.key === 'Escape' && showModal) {
+        setShowModal(false);
+      }
+    },
+    [setShowModal, showModal]
+  );
+
+  useEffect(() => {
+    document.addEventListener('keydown', keyPress);
+
+    return () => document.removeEventListener('keydown', keyPress);
+  }, [keyPress]);
+
   return (
     <>
       {showModal ? (
-        <Background>
-          <ModalWrapper showModal={showModal}>
-            <ModalImg
-              src={require('../image/profile.jpg').default}
-              alt="person"
-            />
-            <ModalContent>
-              <h1>Are you ready?</h1>
-              <p>Get Exclusive access to our next launch</p>
-              <button>Join Now</button>
-            </ModalContent>
-            <CloseModalButton
-              aria-label="Close modal"
-              onClick={() => setShowModal(!showModal)}
-            />
-          </ModalWrapper>
+        <Background ref={modalRef} onClick={closeModal}>
+          <animated.div style={animation}>
+            <ModalWrapper showModal={showModal}>
+              <ModalImg
+                src={require('../image/profile.jpg').default}
+                alt="person"
+              />
+              <ModalContent>
+                <h1>Are you ready?</h1>
+                <p>Get Exclusive access to our next launch</p>
+                <button>Join Now</button>
+              </ModalContent>
+              <CloseModalButton
+                aria-label="Close modal"
+                onClick={() => setShowModal(!showModal)}
+              />
+            </ModalWrapper>
+          </animated.div>
         </Background>
       ) : null}
     </>
